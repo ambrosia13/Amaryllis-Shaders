486bf1dbd07d4854d9f5daa74093eb9086e6e734
ambrosia13 <aavyakumbhare@gmail.com>
2026-07-13 13:23:34 -0700

    checkpoint for jvm crash

--- src/java/config/PostPasses.java
+++ src/java/config/PostPasses.java
@@ -1,19 +1,25 @@
 package config;
 
 import dev.irisshaders.aperture.api.objects.Screen;
+import dev.irisshaders.aperture.api.objects.TextureFormat;
 import dev.irisshaders.aperture.api.pipeline.PipelineConfig;
 import dev.irisshaders.aperture.api.pipeline.ProgramStage;
 import util.SwapTexture2D;
 
 public class PostPasses {
+    public static final int hiDepthMaxLevels = 5; // 0, 2, 4, 6, 8
+    public static final int hiLevelStep = 2; // see hi-z compute shader for details
+
     public static void setup(Screen screen, PipelineConfig pipeline, Atmosphere atmosphere, Gbuffer gbuffer, SwapTexture2D mainTextures) {
+        int hiDepthLevels = hiZPass(screen, pipeline);
+
         // effect pass - for things like reflections and fog
         pipeline.stage(ProgramStage.POST_RENDER)
             .composite("effect", "program/post/effect", "main")
             .overrideObject("inputTexture", mainTextures.read().name())
             .writes("color", mainTextures.write());
         
-        mainTextures.flip();;
+        mainTextures.flip();
 
         // run the exposure metering near the end of teh pipeline
         var exposure = new Exposure(screen, pipeline, mainTextures.read(), mainTextures.write());
@@ -27,5 +33,37 @@ public class PostPasses {
         // combination pass has no explicit outputs and reads from the ping pong
         pipeline.combinationPass("program/post/combination")
             .overrideObject("outputTexture", mainTextures.read().name());
+    }
+
+    // returns count of depth levels
+    static int hiZPass(Screen screen, PipelineConfig pipeline) {
+        // hi-z downsampling pass; downsamples the depth texture in a compute shader (not single-pass)
+        var hiDepthTexture = pipeline.texture2D("hiDepthTexture", TextureFormat.R32_SFLOAT)
+            .renderSize()
+            .usesMipmaps()
+            .create();
+        
+        pipeline.stage(ProgramStage.POST_RENDER)
+            .compute("copyFirstDepth", "program/hiZ", "copyFirstDepth")
+            .dispatch2D(Math.ceilDiv(screen.renderWidth(), 8), Math.ceilDiv(screen.renderHeight(), 8));
+
+        int minDimension = Math.min(screen.renderWidth(), screen.renderHeight());
+        int maxDownsampleLevel = (int) Math.floor(Math.log((double) minDimension) / Math.log(2.0));
+
+        int i;
+
+        for (i = hiLevelStep; i < hiDepthMaxLevels; i++) {
+            int level = i * hiLevelStep;
+
+            if (level > maxDownsampleLevel) break;
+
+            pipeline.stage(ProgramStage.POST_RENDER)
+                .compute("depthDownsample" + level, "program/hiZ", "depthDownsample")
+                .exportInt("srcLod", level - hiLevelStep)
+                .exportInt("dstLod", level)
+                .dispatch2D(Math.ceilDiv(screen.renderWidth() >> level, 8), Math.ceilDiv(screen.renderHeight() >> level, 8));
+        }
+
+        return i;
     }
 }
